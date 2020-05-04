90038d3792bd24fcc1eee08491e08f8287520276
Seth Tisue <seth@tisue.net>
2020-05-04 08:52:04 -0700

    wip

--- modules/core/shared/src/test/scala/eu/timepit/refined/GenericValidateSpec.scala
+++ modules/core/shared/src/test/scala/eu/timepit/refined/GenericValidateSpec.scala
@@ -25,18 +25,6 @@ class GenericValidateSpec extends Properties("GenericValidate") {
     isValid[Equal[Foo.type]](Foo)
   }
 
-  property("Equal.Symbol.isValid") = secure {
-    isValid[Equal[W.`'foo`.T]](Symbol("foo"))
-  }
-
-  property("Equal.Symbol.notValid") = secure {
-    notValid[Equal[W.`'foo`.T]](Symbol("bar"))
-  }
-
-  property("Equal.Symbol.showExpr") = secure {
-    showExpr[Equal[W.`'foo`.T]](Symbol("bar")) ?= "(Symbol(bar) == Symbol(foo))"
-  }
-
   property("Equal.Nat.Int.isValid") = forAll { (i: Int) =>
     isValid[Equal[_0]](i) ?= (i == 0)
   }
